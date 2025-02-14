c7518073bf54cb8ad70bda7d7f089a57c7dc68b4
codechipceo <mansabmir@codechip.in>
2025-02-14 17:16:07 +0530

    Main (#7)
    * update: Unecessary files removed from code
    
    * Update: Typography Component Updated
    
    * Update: Button component Partially fixed
    
    * Tags Branch Merged
    
    * cc-86cxy3r0x-Badges_with_Chips
    
    * Update: BadgeChip updated to Tags
    
    ---------
    
    Co-authored-by: Mansab Mir <mansabmirsde@gmail.com>
    
    * Cc 86cxva26h button component (#6)
    
    * Button component initial implementation
    
    * update: fix button cases
    
    ---------
    
    Co-authored-by: anish2210 <anishjaiswal1220@gmail.com>
    Co-authored-by: Mansab Mir <mansabmirsde@gmail.com>
    
    ---------
    
    Co-authored-by: Mansab Mir <mansabmirsde@gmail.com>
    Co-authored-by: Amit Pattanaik <161040099+amitPattanaik987@users.noreply.github.com>
    Co-authored-by: anish2210 <anishjaiswal1220@gmail.com>

--- libs/shared-ui/src/lib/components/Button/Button.stories.tsx
+++ libs/shared-ui/src/lib/components/Button/Button.stories.tsx
@@ -17,6 +17,7 @@ const meta: Meta<typeof Button> = {
     size: { control: 'select', options: Object.values(ButtonSize) },
     shape: { control: 'select', options: Object.values(ButtonShape) },
     isLoading: { control: 'boolean' },
+    isIcon: { control: 'boolean' },
     disabled: { control: 'boolean' },
   },
 };
@@ -71,5 +72,29 @@ export const Disabled: Story = {
   args: {
     ...Default.args,
     disabled: true,
+  },
+};
+
+export const IconButton: Story = {
+  args: {
+    ...Default.args,
+    isIcon: true,
+    icon: (
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M12 2L15 8H22L16.5 12L18 18L12 14L6 18L7.5 12L2 8H9L12 2Z"
+          style={{
+            fill: 'currentColor',
+            stroke: 'currentColor',
+            strokeWidth: 2,
+          }}
+        />
+      </svg>
+    ),
   },
 };
